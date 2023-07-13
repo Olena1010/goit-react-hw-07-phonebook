512fd6e770c60afc7f944a5f5695263aeb1aef60
Olena1010 <ebojcenko55@gmail.com>
2023-07-13 14:35:10 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Container } from './Container';
+import { BookForm } from './BookForm/BookForm';
+// import { Wrapper } from './BookForm/BookForm.styled';
+import { ContactList } from './ContactList/ContactList';
+import Modal from './Modal/Modal';
+import { NewContactBtn } from './NewContButton/NewContButton';
+import { CloseModalBtn } from './CloseModalButton/CloseModalButton';
+import { Filter } from './Filter/Filter';
+
+
 export const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <GlobalStyle />
+      <h1>Phonebook</h1>
+      <NewContactBtn showModal={toggleModal} />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <CloseModalBtn closeModal={toggleModal} />
+          <BookForm closeModal={toggleModal} />
+        </Modal>
+      )}
+    </Container>
   );
 };
